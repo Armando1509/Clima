aa2b076fd1afa7930cdcebfc0c1e5f992497110f
Armando1509 <armandogomez1509@gmail.com>
2024-05-05 00:02:46 -0600

    funcion para pcultar teclado dando click

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 export default function App() {
+  const ocultarTeclado = ( ) =>{
+    Keyboard.dismiss()
+  }
   return (
     <>
-      
-        <View style={styles.app} >
+      <TouchableWithoutFeedback onPress={()=> ocultarTeclado()} >
+        <View style={styles.app}>
           <View style={styles.contenido}>
             <Formulario />
           </View>
         </View>
-      
+      </TouchableWithoutFeedback>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  contenido:{
-    marginHorizontal: '2.5%'
+  contenido: {
+    marginHorizontal: "2.5%",
   },
-  app:{
+  app: {
     flex: 1,
-    backgroundColor: 'rgb(71,149,212)',
-    justifyContent: 'center'
-  }
+    backgroundColor: "rgb(71,149,212)",
+    justifyContent: "center",
+  },
 });
